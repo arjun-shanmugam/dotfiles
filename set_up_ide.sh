0074811964e9494258c31c08dd06a1f61ca3ce53
Arjun Shanmugam <65803362+arjun-shanmugam@users.noreply.github.com>
2025-04-23 11:02:07 -0400

    put nvim config in .config and .local/state

--- set_up_ide.sh
+++ set_up_ide.sh
@@ -8,8 +8,8 @@ tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 # Update neovim config
 rm -rf /home/arjun.shanmugam/.local/state/nvim
 curl -L -o /home/arjun.shanmugam/.local/state/nvim.tar.gz https://github.com/arjun-shanmugam/dotfiles/raw/refs/heads/main/nvim.tar.gz
-cd /home/arjun.shanmugam/.local/state/
-tar -xzf /home/arjun.shanmugam/.local/state/nvim.tar.gz 
+tar -xzf /home/arjun.shanmugam/.local/state/nvim.tar.gz -C /home/arjun.shanmugam/.local/state
+tar -xzf /home/arjun.shanmugam/.local/state/nvim.tar.gz -C /home/arjun.shanmugam/.config
 cd ~
 
 # Create alias 
