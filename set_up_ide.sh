be835e08e7ff4ced5d5298e6c1b007bae00c2606
Arjun Shanmugam <65803362+arjun-shanmugam@users.noreply.github.com>
2025-04-23 10:51:00 -0400

    extract tar to correct directory

--- set_up_ide.sh
+++ set_up_ide.sh
@@ -8,7 +8,7 @@ tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 # Update neovim config
 rm -rf /home/arjun.shanmugam/.local/state/nvim
 curl -L -o /home/arjun.shanmugam/.local/state/nvim.tar.gz https://github.com/arjun-shanmugam/dotfiles/raw/refs/heads/main/nvim.tar.gz
-tar -xzf /home/arjun.shanmugam/.local/state/nvim.tar.gz
+tar -xzf /home/arjun.shanmugam/.local/state/nvim.tar.gz -C /home/arjun.shanmugam/.local/state/nvim
 
 # Create alias 
 alias nvim='/opt/nvim-linux-x86_64/bin/nvim'
