93c855aac87e49c5960297c73e9c4bfe0774aa36
Arjun Shanmugam <65803362+arjun-shanmugam@users.noreply.github.com>
2025-04-25 13:07:48 -0400

    create config directory manually -- it will not exist until after neovim has been opened for the first time

--- set_up_ide.sh
+++ set_up_ide.sh
@@ -6,7 +6,7 @@ rm -rf /opt/nvim
 tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 
 # Update neovim config
-rm -rf /home/arjun.shanmugam/.local/state/nvim
+mkdir /home/arjun.shanmugam/.local/state/nvim
 curl -L -o /home/arjun.shanmugam/.local/state/nvim.tar.gz https://github.com/arjun-shanmugam/dotfiles/raw/refs/heads/main/nvim.tar.gz
 tar -xzf /home/arjun.shanmugam/.local/state/nvim.tar.gz -C /home/arjun.shanmugam/.local/state
 tar -xzf /home/arjun.shanmugam/.local/state/nvim.tar.gz -C /home/arjun.shanmugam/.config
