28cf317dc20504f968e4d33772fb92daa671c09d
Arjun Shanmugam <65803362+arjun-shanmugam@users.noreply.github.com>
2025-07-10 17:25:09 -0400

    added

--- nvim/lua/arjun-shanmugam/plugins/render-markdown.lua
+++ nvim/lua/arjun-shanmugam/plugins/render-markdown.lua
@@ -1,12 +1,12 @@
--- Register copilot-chat filetype
-require('render-markdown').setup({
-  file_types = { 'markdown', 'copilot-chat' },
-  render_modes = true
-})
-
--- Adjust chat display settings
-require('CopilotChat').setup({
-  highlight_headers = false,
-  separator = '---',
-  error_header = '> [!ERROR] Error',
-})
+return {
+  'MeanderingProgrammer/render-markdown.nvim',
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' },   -- if you use standalone mini plugins
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {},
+  config = function()
+    require('render-markdown').setup({
+      file_types = { 'markdown', 'copilot-chat' },
+    })
+  end
+}
