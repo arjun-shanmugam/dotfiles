704f87238431cc8c4da81e9873d1bb8520aee596
Arjun Shanmugam <65803362+arjun-shanmugam@users.noreply.github.com>
2025-07-10 23:07:30 -0400

    added

--- nvim/lua/arjun-shanmugam/plugins/telescope.lua
+++ nvim/lua/arjun-shanmugam/plugins/telescope.lua
@@ -7,27 +7,7 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   opts = {
-    extensions = {
-      ["ui-select"] = {
-        require("telescope.themes").get_dropdown {
-          -- even more opts
-        }
 
-        -- pseudo code / specification for writing custom displays, like the one
-        -- for "codeactions"
-        -- specific_opts = {
-        --   [kind] = {
-        --     make_indexed = function(items) -> indexed_items, width,
-        --     make_displayer = function(widths) -> displayer
-        --     make_display = function(displayer) -> function(e)
-        --     make_ordinal = function(e) -> string
-        --   },
-        --   -- for example to disable the custom builtin "codeactions" display
-        --      do the following
-        --   codeactions = false,
-        -- }
-      }
-    }
   },
   config = function()
     local telescope = require("telescope")
@@ -46,7 +26,6 @@ return {
       },
     })
 
-    require("telescope").load_extension("ui-select")
     -- set keymaps
     local keymap = vim.keymap
 
