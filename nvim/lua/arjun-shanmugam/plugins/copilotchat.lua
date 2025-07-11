98d72bab65868cc9f4088185b59948b3fdeae89b
Arjun Shanmugam <65803362+arjun-shanmugam@users.noreply.github.com>
2025-07-11 12:55:24 -0400

    added

--- nvim/lua/arjun-shanmugam/plugins/copilotchat.lua
+++ nvim/lua/arjun-shanmugam/plugins/copilotchat.lua
@@ -2,7 +2,17 @@ return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
     dependencies = {
-      { "zbirenbaum/copilot.lua" },                   -- or zbirenbaum/copilot.lua
+      {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        event = "InsertEnter",
+        config = function()
+          require("copilot").setup({
+            --suggestion = { enabled = true },
+            --panel = { enabled = true }
+          })
+        end,
+      },                                              -- or zbirenbaum/copilot.lua
       { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
     },
     build = "make tiktoken",                          -- Only on MacOS or Linux
--- nvim/lua/arjun-shanmugam/plugins/init.lua
+++ nvim/lua/arjun-shanmugam/plugins/init.lua
@@ -24,17 +24,7 @@ return {
     "numToStr/Comment.nvim",
     opts = {},
   },
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup({
-        --suggestion = { enabled = true },
-        --panel = { enabled = true }
-      })
-    end,
-  },
+
   {
     "zbirenbaum/copilot-cmp",
     config = function()
