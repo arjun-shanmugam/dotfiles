714924a06dcb183e30d602abf50b16348814c381
Arjun Shanmugam <65803362+arjun-shanmugam@users.noreply.github.com>
2025-07-11 13:46:53 -0400

    added

--- nvim/lua/arjun-shanmugam/plugins/nvim-cmp.lua
+++ nvim/lua/arjun-shanmugam/plugins/nvim-cmp.lua
@@ -3,22 +3,24 @@ return {
 
   {
     "nvim-cmp",
-    -- dependencies = {
-    --   "hrsh7th/cmp-emoji",
-    --   {
-    --     "zbirenbaum/copilot-cmp",
-    --     opts = {},
-    --   },
-    -- },
-    -- opts = function()
-    --   local cmp = require("cmp")
-    --   return {
-    --     sources = cmp.config.sources({
-    --       { name = "nvim_lsp" },
-    --       { name = "copilot" }
-    --     })
-    --   }
-    -- end,
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+    },
+
+    opts = function()
+      local cmp = require("cmp")
+      return {
+        sources = cmp.config.sources({
+          { name = "lazydev" },
+          { name = "nvim_lsp" },
+          { name = "path" },
+        }, {
+          { name = "buffer" },
+        })
+      }
+    end,
   },
 
 }
