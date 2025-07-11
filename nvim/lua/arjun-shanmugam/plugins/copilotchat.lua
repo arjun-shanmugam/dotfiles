e26b98fc71cd1bd72881d1c982c0e0ab09b9407f
Arjun Shanmugam <65803362+arjun-shanmugam@users.noreply.github.com>
2025-07-11 13:56:42 -0400

    added

--- nvim/lua/arjun-shanmugam/plugins/copilotchat.lua
+++ nvim/lua/arjun-shanmugam/plugins/copilotchat.lua
@@ -8,23 +8,13 @@ return {
         build = ":Copilot auth",
         event = "BufReadPost",
         opts = {
-          suggestion = {
-            enabled = not vim.g.ai_cmp,
-            auto_trigger = true,
-            hide_during_completion = vim.g.ai_cmp,
-            keymap = {
-              accept = false, -- handled by nvim-cmp / blink.cmp
-              next = "<M-]>",
-              prev = "<M-[>",
-            },
-          },
           panel = { enabled = false },
           filetypes = {
             markdown = true,
             help = true,
           },
         },
-      },                                              -- or zbirenbaum/copilot.lua
+      },
       { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
     },
     build = "make tiktoken",                          -- Only on MacOS or Linux
