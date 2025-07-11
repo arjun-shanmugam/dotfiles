b8c25896c29b8942193b256182f564a204183281
Arjun Shanmugam <65803362+arjun-shanmugam@users.noreply.github.com>
2025-07-11 14:02:12 -0400

    added

--- nvim/lua/arjun-shanmugam/plugins/copilotchat.lua
+++ nvim/lua/arjun-shanmugam/plugins/copilotchat.lua
@@ -7,13 +7,23 @@ return {
         cmd = "Copilot",
         build = ":Copilot auth",
         event = "BufReadPost",
-        -- opts = {
-        --   panel = { enabled = false },
-        --   filetypes = {
-        --     markdown = true,
-        --     help = true,
-        --   },
-        -- },
+        opts = {
+          suggestion = {
+            enabled = not vim.g.ai_cmp,
+            auto_trigger = true,
+            hide_during_completion = vim.g.ai_cmp,
+            keymap = {
+              accept = false, -- handled by nvim-cmp / blink.cmp
+              next = "<M-]>",
+              prev = "<M-[>",
+            },
+          },
+          panel = { enabled = false },
+          filetypes = {
+            markdown = true,
+            help = true,
+          },
+        },
       },
       { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
     },
