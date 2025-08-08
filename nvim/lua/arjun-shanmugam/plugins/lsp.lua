04a47c1681bd47224322bb7b7a15907b789a6c24
Arjun Shanmugam <arjun.shanmugam@uber.com>
2025-08-08 16:03:35 -0400

    format on save

--- nvim/lua/arjun-shanmugam/plugins/lsp.lua
+++ nvim/lua/arjun-shanmugam/plugins/lsp.lua
@@ -10,7 +10,7 @@ local lsp_servers = {
   cssls = {},
   emmet_ls = {},
 
-  
+
 
   -- quarto/markdown
   marksman = {
@@ -51,6 +51,20 @@ return {
     "nvimtools/none-ls.nvim",
     config = function()
       require("null-ls").setup({
+        on_attach = function(client, bufnr)
+          if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              group = augroup,
+              buffer = bufnr,
+              callback = function()
+                -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+                vim.lsp.buf.format({ async = false })
+              end,
+            })
+          end
+        end,
         sources = {
           require("null-ls").builtins.formatting.prettierd,
           require("null-ls").builtins.diagnostics.markdownlint,
