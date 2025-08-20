c43766db206eb8dc9479967acc39b32f4eae700e
Arjun Shanmugam <arjun.shanmugam@uber.com>
2025-08-20 09:51:59 -0400

    new dsw setup script

--- set_up_dsw.sh
+++ set_up_dsw.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-source $VIRTUAL_ENV_DIR/python310/bin/activate ; source ~/.path ; pyenv deactivate ; source $VIRTUAL_ENV_DIR/python310/bin/activate ; ~/arjun.shanmugam_nfs/dotfiles/install ; source $VIRTUAL_ENV_DIR/python310/bin/activate 
+deactivate ; pyenv deactivate ; ~/arjun.shanmugam_nfs/dotfiles/install ; source $VIRTUAL_ENV_DIR/python310/bin/activate ; source ~/.path
